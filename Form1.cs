9b9b46ee0a0870908428657fb64bc6f73f88ec41
Daniil Knaub <andy.typing@mail.ru>
2026-04-07 12:04:48 +0800

    Added vector velocity

--- Form1.cs
+++ Form1.cs
@@ -63,16 +63,18 @@ namespace SixthLabWorkApp
                     particle.X = MousePositionX;
                     particle.Y = MousePositionY;
 
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
                     particle.Radius = 2 + Particle.rand.Next(10);
                 }
                 else
                 {
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
                 }
             }
 
@@ -101,7 +103,6 @@ namespace SixthLabWorkApp
         {
             foreach (var particle in particles)
             {
-                var directionInRadians = particle.Direction / 180 * Math.PI;
                 particle.Draw(g);
             }
         }
--- Particle.cs
+++ Particle.cs
@@ -9,8 +9,8 @@ namespace SixthLabWorkApp
         public float X;
         public float Y;
 
-        public float Direction;
-        public float Speed;
+        public float SpeedX;
+        public float SpeedY;
         
         public float Life;
 
@@ -18,8 +18,12 @@ namespace SixthLabWorkApp
 
         public Particle()
         {
-            Direction = rand.Next(360);
-            Speed = 1 + rand.Next(10);
+            var direction = (double) rand.Next(360);
+            var speed = 1 + rand.Next(10);
+
+            SpeedX = (float) (Math.Cos(direction / 180 * Math.PI) * speed);
+            SpeedY = (float) (Math.Sin(direction / 180 * Math.PI) * speed);
+
             Radius = 2 + rand.Next(10);
             Life = 20 + rand.Next(100);
         }
